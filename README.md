07f0e10ed62006d82806f6b0fcbf7516ed22c0b5
unjungcoding <teven1103@naver.com>
2021-07-17 16:04:28 +0900

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+# InstaClone-Web 
+* practicing Color on White Surface design 
+
+-Router [ ]
+-Authentication [ ]
+-Architechture [ ]
+-dark mode [ ]
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { HashRouter as Router, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+      <Route>
+        
+      </Route>
+    </Router>
+  );
+}
+
+export default App;
